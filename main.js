9b40f023cb440604141a258e531561fc2c5f9b1a
Laqah <laqahraca@protonmail.com>
2021-07-30 18:10:00 +0900

    파일 생성을 통한 글 생성 기능

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
-var url = require('url'); //요구한다. url이라는 모듈.
+var url = require('url'); //요구한다. url이라는 nodejs의 모듈.
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -44,9 +45,9 @@ var app = http.createServer(function(request,response){
   if(pathname === '/') {
     if(queryData.id === undefined) {
 
-      fs.readdir('./data', function(error, filelist) {
+      fs.readdir('./data', 'utf8', function(error, filelist) {
 
-        var title = 'Welcome';
+        var title = 'Welcome???';
         var description = 'Hello, Node.js';
         var list = templateList(filelist);
 
@@ -59,7 +60,7 @@ var app = http.createServer(function(request,response){
 
     } else {
 
-        fs.readdir('./data', function(error, filelist) {
+        fs.readdir('./data', 'utf8', function(error, filelist) {
 
         fs.readFile(`data/${title}`, 'utf8', function(err, description){
           var list = templateList(filelist);
@@ -75,13 +76,13 @@ var app = http.createServer(function(request,response){
     }
   }
   else if(pathname === '/create') {
-    fs.readdir('./data', function(error, filelist) {
+    fs.readdir('./data', 'utf8', function(error, filelist) {
 
       var title = 'WEB - create';
       var list = templateList(filelist);
 
       var template = templateHTML(title, list, `
-        <form action="/process_create" method="post">
+        <form action="/create_process" method="post">
           <p><input type="text" name="title" placeholder="title"></p>
           <p>
             <textarea name="description" placeholder="description"></textarea>
@@ -94,6 +95,28 @@ var app = http.createServer(function(request,response){
       response.writeHead(200); //파일을 성공적으로 전송했다.
       response.end(template);
     });
+  }
+  else if(pathname === '/create_process') {
+    var body = '';
+
+    //이벤트. 데이터를 가져올 수도 있고, 정보를 객체화 할 수도 있다.
+    request.on('data', function(data){ //정보가 조각조각 들어올
+      body = body + data;
+
+      //보안장치로 body가 너무 크면 연결을 끊을 수도 있다.
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+
+      fs.writeFile(`data/${title}`, description, 'utf8',
+      function(err){
+        response.writeHead(302, {Location: `/?id=${qs.escape(title)}`}); //다른 곳으로 일시적 리다이렉.
+        response.end();
+      })
+    });
+
   }
   else {
     response.writeHead(404); //404: 파일을 찾을 수 없다.
