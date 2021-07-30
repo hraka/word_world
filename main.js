e24e98babf4055fbef99dbffb799f0d0518610ed
laqah <laqahraca@protonmail.com>
2021-07-30 16:15:37 +0900

    create process - 버튼 만들기

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function templateHTML(title, list, body) {
       <h1><a href="/">언어사전</a></h1>
 
       ${list}
+      <a href="/create">create</a>
       ${body}
 
     </body>
@@ -35,30 +36,30 @@ function templateList(filelist){
 }
 
 var app = http.createServer(function(request,response){
-    var _url = request.url;
-    var queryData = url.parse(_url, true).query;
-    var pathname = url.parse(_url, true).pathname;
-    var title = queryData.id;
+  var _url = request.url;
+  var queryData = url.parse(_url, true).query;
+  var pathname = url.parse(_url, true).pathname;
+  var title = queryData.id;
 
-    if(pathname === '/') {
-      if(queryData.id === undefined) {
+  if(pathname === '/') {
+    if(queryData.id === undefined) {
 
-        fs.readdir('./data', function(error, filelist) {
+      fs.readdir('./data', function(error, filelist) {
 
-          var title = 'Welcome';
-          var description = 'Hello, Node.js';
-          var list = templateList(filelist);
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = templateList(filelist);
 
-          var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
-          response.writeHead(200); //파일을 성공적으로 전송했다.
-          response.end(template);
-        });
+        var template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
+        response.writeHead(200); //파일을 성공적으로 전송했다.
+        response.end(template);
+      });
 
 
 
     } else {
 
-      fs.readdir('./data', function(error, filelist) {
+        fs.readdir('./data', function(error, filelist) {
 
         fs.readFile(`data/${title}`, 'utf8', function(err, description){
           var list = templateList(filelist);
@@ -67,12 +68,33 @@ var app = http.createServer(function(request,response){
 
           response.writeHead(200); //파일을 성공적으로 전송했다.
           response.end(template);
+        });
       });
-    });
 
 
+    }
   }
-}
+  else if(pathname === '/create') {
+    fs.readdir('./data', function(error, filelist) {
+
+      var title = 'WEB - create';
+      var list = templateList(filelist);
+
+      var template = templateHTML(title, list, `
+        <form action="/process_create" method="post">
+          <p><input type="text" name="title" placeholder="title"></p>
+          <p>
+            <textarea name="description" placeholder="description"></textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+      `);
+      response.writeHead(200); //파일을 성공적으로 전송했다.
+      response.end(template);
+    });
+  }
   else {
     response.writeHead(404); //404: 파일을 찾을 수 없다.
     response.end('Not found');
